43fa841d261f888db012e3c36d02fb46043c4730
rdrpenguin04 <rdrpenguin04@users.noreply.github.com>
2017-11-14 14:43:11 -0600

    Update MULE.js
    Created better infrastructure for game running, added function for title script (Script id 0x00 = 0).

--- MULE.js
+++ MULE.js
@@ -1,6 +1,11 @@
 var script;
+var resourcesToLoad;
+var loadedResources;
 
 function init() {
+  resourcesToLoad = []; /* TODO: put in resources to load, this is any images or game data
+                        that we don't want to embed in this file. */
+  loadedResources = [];
   script = 1;
   window.requestAnimationFrame = window.requestAnimationFrame
     || window.mozRequestAnimationFrame
@@ -15,22 +20,42 @@ function init() {
   window.requestAnimationFrame(tick);
 }
 
+function title() {
+  
+}
+
+var audio;
+
 var doneLoading = true;
 function loadDataAndStartMusic() {
   //Load data here
   //If we are done, start music and set the current script to 0 (title)
   if(doneLoading) {
-    var audio = new Audio("mainTheme.wav");
+    audio = new Audio("mainTheme.wav");
     audio.play();
     audio.loop = true;
-    script = 0;
+    script = 0; //title
   }
+}
+
+function stop() {
+  audio.stop();
+  //Unload resources here.
 }
 
 function tick() {
   switch(script) {
-  case 1:
-    loadDataAndStartMusic();
+    case 0:
+      title();
+      break;
+    case 1:
+      loadDataAndStartMusic();
+      break;
+    default:
+      log("Invalid game state, restarting...");
+      stop();
+      init();
+      break;
   }
   window.requestAnimationFrame(tick);
 }
